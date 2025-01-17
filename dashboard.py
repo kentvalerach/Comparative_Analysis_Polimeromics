b9dae7a929f1b56b6dbe8d2ba4fe4d1319edcbff
Kent Valera C. <121522633+kentvalerach@users.noreply.github.com>
2025-01-17 19:15:59 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -198,5 +198,5 @@ def update_dashboard(prev_clicks, next_clicks, current_index):
     return f"Current index: {new_index}", biogrid_details, rcsb_details, comparison_plot_1, comparison_plot_2
 
 # Run the app
-if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8000)
+port = int(os.environ.get("PORT", 8000))
+app.run_server(debug=False, host='0.0.0.0', port=port)
