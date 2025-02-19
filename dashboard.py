54801c3fcc6ae660d3d8dbaa7cdf928706437635
Kent Valera C. <121522633+kentvalerach@users.noreply.github.com>
2025-02-19 21:06:04 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -101,11 +101,11 @@ app.layout = html.Div([
                     html.Li("**Precision Medicine**: Supporting personalized therapeutic approaches by linking structural "
                             "variations to specific genetic markers.")
                 ]),
-                html.P("This is an independent Bioinformatics study by Kent Valera Chirinos"
+                html.P("This is an independent Bioinformatics study by Kent Valera Chirinos.  "
                 "The project combines data from two main sources: BIOGRID a database focused on molecular interactions," 
-                " in particular genes and proteins relevant to Homo sapiens. RCSB PDB repository of structural and biophysical" 
+                " in particular genes and proteins relevant to Homo sapiens. RCSB PDB a repository of structural and biophysical" 
                 " data on macromolecules, including proteins and nucleic acids. You can access the scripts and data"),
-                html.A(" in the repository" , href="https://github.com/kentvalerach"),           
+                html.A(" Kent Valera chirinos, repository" , href="https://github.com/kentvalerach"),           
             ], style={'border': '1px solid black', 'padding': '10px', 'marginTop': '20px'})
         ], style={'width': '45%', 'float': 'right', 'padding': '10px'})
     ], style={'display': 'flex', 'justifyContent': 'space-between'})
